789b2baadd215593764a1a4d10a63fefd3f3363c
Mustakim <Mustakim@mustakims-mbp>
2017-01-18 12:35:14 +0000

    Adding degrep package

---
+++ GMSIS/src/degrep/ComponentLoader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package degrep;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Mustakim
+ */
+public class ComponentLoader {
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "hello");
+    }
+}
