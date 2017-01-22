43946242e5d4cb53de983b5faa217b37fcf3d222
Manoharan <Manoharan@192.168.1.8>
2017-01-22 20:16:00 +0000

    Adding Customer Package

--- GMSIS/src/Customer/ComponentLoader.java
+++ GMSIS/src/Customer/ComponentLoader.java
@@ -11,9 +11,41 @@ package Customer;
  */
 public class ComponentLoader {
     
+    private String firstname;
+    private String surname;
+    private String address;
+    private String postcode;
+    private String phone;
+    private String email;
+    private boolean check;
+    
+    public ComponentLoader(String fName, String lName, String addr, String pCode, String phone_no, String email_addr)
+    {
+        firstname = fName;
+        surname = lName;
+        address = addr;
+        postcode = pCode;
+        phone = phone_no;
+        email = email_addr;
+    }   
+    
+    //public static void makeBooking(Vehicle vehicle)
+  
+        // linked to Diagnostic and Repairs
+    //}
+    
+    public static void payBill()
+    {
+    
+    }
+    
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args)
     {
-        
+        System.out.println("HELLO");
     }
     
 }
