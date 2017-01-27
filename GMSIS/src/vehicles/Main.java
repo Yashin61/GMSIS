82df3e8827e2b6b19ebb3cea3c0d77cfe6292849
150479358 <y.hosseinkhorrami@se15.qmul.ac.uk>
2017-01-27 13:42:29 +0000

    I deleted the old package and created new one with its sub packages.

---
+++ GMSIS/src/vehicles/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicles;
+import customers.*;
+/**
+ *
+ * @author yhk30
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
