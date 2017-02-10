b4c248d5140c7c6a747a318b0c65a5128b907f33
150479358 <y.hosseinkhorrami@se15.qmul.ac.uk>
2017-02-10 17:38:39 +0000

    I added Database.java and put common_guiController.java

--- GMSIS/src/common/Database.java
+++ GMSIS/src/common/Database.java
@@ -5,10 +5,29 @@
  */
 package common;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
- * @author prashant
+ * @author yhk30
  */
-public class Database {
+public class Database
+{
+    private static Connection connection;
     
+    private Database() throws ClassNotFoundException, SQLException
+    {
+        Class.forName("org.sqlite.JDBC");
+        connection=DriverManager.getConnection("jdbc:sqlite:src/common/Records.db");
+        System.out.print("Connected to database!");
+    }
+    
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        if(connection == null){
+            new Database();
+        }
+        return connection;
+    }
 }
