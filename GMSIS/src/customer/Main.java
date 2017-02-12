d3a85fc3320b27ba237e0d2bd6de5e87b0bd6eb7
Nandhini Manoharan <n.manoharan@se15.qmul.ac.uk>
2017-02-12 22:58:02 +0000

    Main method

--- GMSIS/src/customer/Main.java
+++ GMSIS/src/customer/Main.java
@@ -9,6 +9,26 @@ package customer;
  *
  * @author Manoharan
  */
-public class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("gui/CustomerEdit.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }
