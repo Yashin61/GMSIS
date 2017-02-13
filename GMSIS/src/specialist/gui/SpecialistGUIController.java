044ce3a7c062a5ebe49846a44374bb1746241026
Prashant Ghale <p.ghale@se15.qmul.ac.uk>
2017-02-13 23:23:08 +0000

    making the gui for the specialist module. 3 pages - a draft of addSPC, editSPC and specialistGUI has been made. spcRepairer is still in progress as there are some requirements that needs to clarified

---
+++ GMSIS/src/specialist/gui/SpecialistGUIController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package specialist.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author prashant
+ */
+public class SpecialistGUIController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
