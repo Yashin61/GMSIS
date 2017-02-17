83ce34aaeb833fe4653bfbacdc790eec04842046
Rohim Uddin <ec14125@qmul.ac.uk>
2017-02-17 19:26:15 +0000

    I made my packages start with lower case.

---
+++ GMSIS/src/parts/Parts.java
@@ -0,0 +1,42 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parts;
+/**
+ *
+ * @author rohim
+ */
+
+public class Parts {
+ 
+    int ID;
+   
+    
+    public Parts(int pid){
+    ID = pid;
+    
+    }
+    
+    public void addStock(int newqty){
+        ConnectionToParts c = new ConnectionToParts();
+        
+        c.add(ID,newqty);
+          //  qty= qty+newqty;
+    }
+    
+    public void partWithdraw(){
+       //qty=qty-1; 
+    }
+
+    
+    public void SeachByID(){
+        ConnectionToParts con = new ConnectionToParts();
+        con.IDSearch(ID);
+    }
+ 
+
+ 
+ }
