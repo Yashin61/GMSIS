225458e734dfb6832ac9453350aa5c4ae52c9ce8
Prashant Ghale <p.ghale@se15.qmul.ac.uk>
2017-02-20 17:27:12 +0000

    made changes to the login page. Also added in login table in the records

--- GMSIS/src/common/Main.java
+++ GMSIS/src/common/Main.java
@@ -18,14 +18,15 @@ import javafx.stage.Stage;
  */
 public class Main extends Application
 {
-    
     @Override
-    public void start(Stage stage) throws Exception
-    {
-        Parent root = FXMLLoader.load(getClass().getResource("Logging.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        
         Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        
+        primaryStage.setTitle("GMSIS Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
     public static void main(String[] args)
