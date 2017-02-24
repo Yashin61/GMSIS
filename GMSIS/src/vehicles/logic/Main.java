9af1ff707cf1a77bec0077a0e89791c4c92c6a2f
150479358 <=>
2017-02-24 15:54:09 +0000

    Revert "Modified Main.java"
    This reverts commit 474b76856209ea69d710b91d937fb47827501802.

--- GMSIS/src/vehicles/logic/Main.java
+++ GMSIS/src/vehicles/logic/Main.java
@@ -14,9 +14,8 @@ public class Main extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("/vehicles/gui/VehiclePage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/vehicles/gui/VehicleGUI.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Vehicle Records");
         stage.setScene(scene);
         stage.show();
     }
